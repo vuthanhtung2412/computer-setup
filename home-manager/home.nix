277fcc5c17f50cf2a995abe4c71a7c1341738694
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-10-18 10:28:49 +0200

    style tmux catppuccin

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -289,6 +289,32 @@ in {
     tmux = {
       enable = true;
       mouse = true;
+      catppuccin = {
+        enable = true;
+        extraConfig = ''
+        # This is because version of catppuccin tmux extensions of nix pkgs < v0.3.0
+        set -g @catppuccin_window_left_separator ""
+        set -g @catppuccin_window_right_separator " "
+        set -g @catppuccin_window_middle_separator " █"
+        set -g @catppuccin_window_number_position "right"
+
+        set -g @catppuccin_window_default_fill "number"
+        set -g @catppuccin_window_default_text "#W"
+
+        set -g @catppuccin_window_current_fill "number"
+        set -g @catppuccin_window_current_text "#W"
+
+        set -g @catppuccin_date_time_text "%H:%M"
+        set -g @catppuccin_status_modules_right "directory user host session date_time battery"
+
+        set -g @catppuccin_status_left_separator  " "
+        set -g @catppuccin_status_right_separator ""
+        set -g @catppuccin_status_fill "icon"
+        set -g @catppuccin_status_connect_separator "no"
+
+        set -g @catppuccin_directory_text "#{pane_current_path}"
+        '';
+      };
       plugins = with pkgs.tmuxPlugins; [
         sensible
         vim-tmux-navigator
@@ -300,7 +326,9 @@ in {
       shell = "${pkgs.zsh}/bin/zsh";
       keyMode = "vi";
       extraConfig = ''   
+      # Correct color display (ex: Neovim catppuccin)
       set-option -sa terminal-overrides ",xterm*:Tc"
+
       # Ctrl b is still my preferred prefix
 
       # Shift arrow to switch windows
