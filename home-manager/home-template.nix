96c70438d9492745ec870776fd78e3b002428ac0
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-10-22 23:27:49 +0200

    yazi function already exists as

--- home-manager/home-template.nix
+++ home-manager/home-template.nix
@@ -283,16 +283,6 @@ in {
           fzf_bin=$(which fzf)
           zvm_after_init_commands+=("eval \"\$($fzf_bin --zsh)\"")
         fi
-
-        # Initiallize yazi
-        function y() {
-          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-          yazi "$@" --cwd-file="$tmp"
-          if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-            builtin cd -- "$cwd"
-          fi
-          rm -f -- "$tmp"
-        }
       '';
       initExtraFirst = ''
         alias gc='gcloud'
