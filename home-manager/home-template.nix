b7657daa5c8e2c6a6a8673156b4cfa89b8cbe1ff
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-10-25 12:58:02 +0200

    more comments on why need fd and escape nix var

--- home-manager/home-template.nix
+++ home-manager/home-template.nix
@@ -113,7 +113,7 @@ in {
     yq
     ffmpeg
     yt-dlp
-    fd # this is added since many programs such as yazi and ulauncher depends on it to find files and dir
+    fd # this is added since many programs such as yazi, fzf and ulauncher depends on it to find files and dir
     ripgrep
     thefuck
     tldr
@@ -302,7 +302,7 @@ in {
         # disable sort when completing `git checkout`
         zstyle ':completion:*:git-checkout:*' sort false
         zstyle ':completion:*:descriptions' format '[%d]'
-        zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+        zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS} # Example of how to escape nix variable
         zstyle ':completion:*' menu no
         zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
         zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
