cc4da3b8092ba4ff5a6a4bf15db076dae10b5fce
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-12-06 04:10:14 +0100

    track backed up files

--- Linux/sripts/hms.sh
+++ Linux/sripts/hms.sh
@@ -9,4 +9,10 @@ cd "$parent_path"
 
 ./make_hm.sh
 export NIXPKGS_ALLOW_UNFREE=1
-home-manager switch --impure --show-trace --flake . -b backup
+home-manager switch --impure --show-trace --flake . -b backup | tee hms.log
+
+# Track backed up files 
+cat hms.log | grep -o "Existing file '[^']*'" | cut -d"'" -f2 >> ../../backed_up_files.txt
+rm hms.log
+sort -uo ../../backed_up_files.txt ../../backed_up_files.txt
+
