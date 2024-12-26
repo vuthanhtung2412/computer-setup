e9df94bee17a85f5103f34e9265dc3d27905d6e0
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-12-27 00:02:32 +0100

    migrate vimrc and mise

--- modules/programming_langs.nix
+++ modules/programming_langs.nix
@@ -5,6 +5,7 @@
 }:
 {
   home.packages = with pkgs; [
+    mise
     (python311.withPackages (
       ps: with ps; [
         # this is solely for albert plugins installation, and it also make sense since python is a scripting languages
@@ -13,24 +14,6 @@
     ))
     nixfmt-rfc-style # not available in mason yet
   ];
-  programs = {
-    mise = {
-      # `mise implode` to purge every thing
-      # `mise prune` to gc (state of every cofig file is kept in `~/.local/state/mise/tracked-configs` so it is good)
-      enable = true;
-      globalConfig = {
-        tools = {
-          python = "3.12";
-          java = "21";
-          rust = "1.81";
-          go = "1.22";
-          uv = "0.5";
-          node = "20.18";
-        };
-        env = {
-          CARGO_TARGET_DIR = "target";
-        };
-      };
-    };
-  };
+
+  home.file.".config/mise/config.toml".source = ../dotfiles/linux/dot_config_mise_config.toml;
 }
