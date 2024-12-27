d45dcf23f68695a7388b93997332ff5b740b8147
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-12-27 14:53:38 +0100

    add check logic to dotfiles

--- dotfiles/migration/chezmoi_apply_dotfiles.sh
+++ dotfiles/migration/chezmoi_apply_dotfiles.sh
@@ -1,3 +1,5 @@
+# apply all tracked files
+
 # chezmoi apply ~/.bashrc
 # chezmoi apply ~/.vimrc
 # chezmoi apply ~/.zshrc
@@ -13,4 +15,24 @@
 # chezmoi apply ~/.config/yazi
 # chezmoi apply ~/.config/Code/extensions/.vscode/extensions.json
 # chezmoi apply ~/.zsh_plugins.txt
+
+# List all managed files by chezmoi
+# chezmoi managed
 chezmoi -v apply
+
+# check
+ls -la ~/.bashrc
+ls -la ~/.vimrc
+ls -la ~/.zshrc
+ls -la ~/.config/bat/config
+ls -la ~/.config/Code/User/settings.json
+ls -la ~/.config/git/config
+ls -la ~/.config/kitty/kitty.conf
+ls -la ~/.config/lazygit/config.yml
+ls -la ~/.config/mise/config.toml
+ls -la ~/.config/oh-my-posh/config.json
+ls -la ~/.config/tmux/tmux.conf
+ls -la ~/.config/kitty/theme.conf
+ls -la ~/.config/yazi
+ls -la ~/.config/Code/extensions/.vscode/extensions.json
+ls -la ~/.zsh_plugins.txt
