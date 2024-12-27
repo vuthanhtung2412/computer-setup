77f650e38a836233ee8768eecd8e2714acb5cfc4
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-12-27 14:48:34 +0100

    chezmoi apply dotfiles

---
+++ dotfiles/migration/chezmoi_apply_dotfiles.sh
@@ -0,0 +1,16 @@
+# chezmoi apply ~/.bashrc
+# chezmoi apply ~/.vimrc
+# chezmoi apply ~/.zshrc
+# chezmoi apply ~/.config/bat/config
+# chezmoi apply ~/.config/Code/User/settings.json
+# chezmoi apply ~/.config/git/config
+# chezmoi apply ~/.config/kitty/kitty.conf
+# chezmoi apply ~/.config/lazygit/config.yml
+# chezmoi apply ~/.config/mise/config.toml
+# chezmoi apply ~/.config/oh-my-posh/config.json
+# chezmoi apply ~/.config/tmux/tmux.conf
+# chezmoi apply ~/.config/kitty/theme.conf
+# chezmoi apply ~/.config/yazi
+# chezmoi apply ~/.config/Code/extensions/.vscode/extensions.json
+# chezmoi apply ~/.zsh_plugins.txt
+chezmoi -v apply
