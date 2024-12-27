d91bea970edf88f9b22674ca24e8da45f92d3c84
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-12-27 23:57:32 +0100

    clean tmp dotfiles for migration

--- modules/linux_term_emulators.nix
+++ modules/linux_term_emulators.nix
@@ -8,6 +8,6 @@
     (config.lib.nixGL.wrap warp-terminal)
     (config.lib.nixGL.wrap pkgs.kitty)
   ];
-  home.file.".config/kitty/kitty.conf".source = ../dotfiles/linux/dot_config_kitty_kitty.conf;
-  home.file.".config/kitty/theme.conf".source = ../dotfiles/linux/dot_config_kitty_theme.conf;
+  # home.file.".config/kitty/kitty.conf".source = ../dotfiles/linux/dot_config_kitty_kitty.conf;
+  # home.file.".config/kitty/theme.conf".source = ../dotfiles/linux/dot_config_kitty_theme.conf;
 }
