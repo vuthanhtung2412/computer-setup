e2af3b47e8cc3f3f6b17fa1c760c3742b6bde7e6
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-12-29 21:10:27 +0100

    remove python since it doesn't have to support albert

--- modules/Mac/programming_langs.nix
+++ modules/Mac/programming_langs.nix
@@ -7,12 +7,6 @@
   home.packages = with pkgs; [
     # TODO: mise zsh competion is not working
     mise
-    (python311.withPackages (
-      ps: with ps; [
-        # this is solely for albert plugins installation, and it also make sense since python is a scripting languages
-        pip
-      ]
-    ))
     nixfmt-rfc-style # not available in mason yet
   ];
 
